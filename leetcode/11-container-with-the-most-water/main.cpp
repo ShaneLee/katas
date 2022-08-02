9d182dedf2a96b1c18d49e0aee4d27cf00d21160
Shane <me@shanel.ee>
2022-08-02 21:21:12 +0100

    Add leetcode 11 container with the most water

---
+++ leetcode/11-container-with-the-most-water/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {  
+      int l = 0;
+      int r = height.size() -1;
+
+      int res = 0;
+
+      while (l < r) {
+        int area = (r - l) * min(height[l], height[r]);
+        res = max(area, res);
+
+        if (height[l] < height[r]) {
+          l+=1;
+        }
+        else {
+          r-=1;
+        }
+
+      }
+
+      return res;
+        
+    }
+};
